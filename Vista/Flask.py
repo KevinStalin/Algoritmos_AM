520e82937a8c5edfa014c5f1578d5886b9bc8e69
kevinmania4 <stalram98@gmail.com>
2020-07-31 21:02:14 -0500

    comienzo con flask

--- Vista/Flask.py
+++ Vista/Flask.py
@@ -1,8 +1,15 @@
 from flask import Flask, render_template
 from flask import make_response
 from flask import request, jsonify
+
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/",methods=['GET', 'POST'])
 def home():
-    return "hi"
+    return render_template('index.html')
+
+
+
+
+if __name__== '__main__':
+    app.run(debug=True)
