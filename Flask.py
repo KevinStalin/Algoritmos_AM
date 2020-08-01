619e5fc9b74ef92b73a98b1ce96069a1c729d1aa
Eduardo-Quisupangui <qsanty_666@hotmail.es>
2020-08-01 00:16:37 -0500

    botones

--- Flask.py
+++ Flask.py
@@ -2,23 +2,35 @@ from flask import Flask, render_template
 from flask import make_response
 from flask import request, jsonify
 
+
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template('prueba.html')
 
+@app.route('/ranfore')
+def primera():
+    
+    return render_template('RandomForest.html')
+
+@app.route('/navyes')
+def segunda():
+    return render_template('NaiveBayes.html')
+
+@app.route('/reglog')
+def tercera():
+    return render_template('RegresionLLogistica.html')
+
+@app.route('/redneu')
+def cuarta():
+    return render_template('RedNeuronal.html')
+
+@app.route('/msv')
+def quinta():
+    return render_template('SoporteVectorial.html')
 
 
 
 if __name__== '__main__':
     app.run(debug=True)
-
-# server_name = app.config['SERVER_NAME']
-# if server_name and ':' in server_name:
-#     host, port = server_name.split(":")
-#     port = int(port)
-# else:
-#     port = 5000
-#     host = "localhost"
-# app.run(host=host, port=port)
